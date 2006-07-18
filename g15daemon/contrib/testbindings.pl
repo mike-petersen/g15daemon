ae5bde1146386f1a62a6420facbe2833f9db190b
mlampard <mlampard@ab83d0d9-5618-0410-a047-8efd564e0644>
2006-07-18 15:58:20 +0000

    improve the perl bindings somewhat.  make them more perly
    git-svn-id: https://g15daemon.svn.sourceforge.net/svnroot/g15daemon/trunk@11 ab83d0d9-5618-0410-a047-8efd564e0644

--- g15daemon/contrib/testbindings.pl
+++ g15daemon/contrib/testbindings.pl
@@ -1,18 +1,34 @@
-use GD;
 use G15Daemon;
 
+use GD;
+#use GD::Graph::lines;
+use GD::Graph::bars;
+
+# simple demo. creates two screens (selectable via L1 button) and 
+# displays some text & a border on one, and a graph on the other..
 
 
-printf "perl bindings version is %f\n",G15Daemon->VERSION;
-printf "interface version is %f\n ",G15Daemon->apiversion;
+@data = (
+    ["1st","2nd","3rd","4th","5th","6th","7th", "8th", "9th"],
+    [    1,    2,    5,    6,    3,  1.5,    1,     3,     4],
+    [ sort { $a <=> $b } (1, 2, 5, 6, 3, 1.5, 1, 3, 4) ]
+);
 
-my $width = G15Daemon->width();
-my $height = G15Daemon->height();
+# possible screentypes are $g15pbuf (1bpp pixel buffer) 
+# $g15txt (text - not implemented yet) 
+# or $g15wbmp (WBMP pixelbuffer useful for GD)
+#
+# ok.. create a couple of new screens..
+my $lcdintro = G15Daemon->new($g15wbmp);
+my $lcdgraph = G15Daemon->new($g15wbmp);
+                                           
+printf "perl bindings version is %f\n",$lcdintro->VERSION;
+printf "interface version is %f\n ",$lcdintro->apiversion;
 
-# possible screentypes are 0 (1bpp pixel buffer) 1 (text - not implemented yet) or 2 (WBMP pixelbuffer)
-my $a = G15Daemon->newscreen(2);
+my $width = $lcdintro->width();
+my $height = $lcdintro->height();
 
-# create a new image
+# create a new GD image
 $im = new GD::Image ($width, $height);
 
 # allocate some colors - these are the only valid ones for the G15
@@ -32,13 +48,36 @@ $im->rectangle (10, 10, 150, 33, $white);
 # fill it
 $im->fill(12,12,$white);
 $im->fill(145,12,$white);
-
+# draw some text onto the buffer
 $im->string(gdSmallFont,30,15,"G15Daemon & GDlib",$black);
 
+#convert the buffer to WBMP format..
 my $wbmpdata = $im->wbmp ($black);
-G15Daemon->send($a,$wbmpdata , length($wbmpdata));
+#send it to the daemon....
+$lcdintro->send($wbmpdata);
+
+# create a graph, and send it on the other connection.
+my $graph = GD::Graph::bars->new(160, 44);
+$graph->set(
+#         x_label           => 'X Label',
+#         y_label           => 'Y label',
+#         title             => 'Some simple graph',
+         y_max_value       => 8,
+         y_tick_number     => 8,
+         y_label_skip      => 5,
+         x_label_skip      => 2,
+         dclrs => [ qw(dblue) ]
+) or die $my_graph->error;
+
+my $gd = $graph->plot(\@data) or die $graph->error;                                                                                       
+
+# again convert to WBMP format and send it...
+my $graphwbmp = $gd->wbmp ($black);
+$lcdgraph->send($graphwbmp);
 
 sleep(60);
-G15Daemon->closescreen($a);
+
+$lcd->closescreen();
+$lcdgraph->closescreen();
 
 
