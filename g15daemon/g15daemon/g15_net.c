882d91c1cea385b264b1114bfae10383b1a8519d
mlampard <mlampard@ab83d0d9-5618-0410-a047-8efd564e0644>
2006-11-23 06:55:46 +0000

    bugfix: stop the daemon going into a tailspin and pegging cpu if unexpected data is recieved
    git-svn-id: https://g15daemon.svn.sourceforge.net/svnroot/g15daemon/trunk@193 ab83d0d9-5618-0410-a047-8efd564e0644

--- g15daemon/g15daemon/g15_net.c
+++ g15daemon/g15daemon/g15_net.c
@@ -176,15 +176,19 @@ int g15_send(int sock, char *buf, unsigned int len)
     while(total < len && !leaving) {
         memset(pfd,0,sizeof(pfd));
         pfd[0].fd = sock;
-        pfd[0].events = POLLOUT;
+        pfd[0].events = POLLOUT | POLLERR | POLLHUP | POLLNVAL;
         if(poll(pfd,1,500)>0) {
-            if(pfd[0].revents & POLLOUT) {
+            if(pfd[0].revents & POLLOUT && !(pfd[0].revents & POLLERR || pfd[0].revents & POLLHUP || pfd[0].revents & POLLNVAL)) {
                 retval = send(sock, buf+total, bytesleft, 0);
                 if (retval == -1) { 
                     break; 
                 }
                 bytesleft -= retval;
                 total += retval;
+            }
+            if((pfd[0].revents & POLLERR || pfd[0].revents & POLLHUP || pfd[0].revents & POLLNVAL)){
+                retval=-1;
+                break;
             }
         }
     }
@@ -204,9 +208,9 @@ int g15_recv(lcdnode_t *lcdnode, int sock, char *buf, unsigned int len)
     while(total < len  && !leaving) {
         memset(pfd,0,sizeof(pfd));
         pfd[0].fd = sock;
-        pfd[0].events = POLLIN | POLLPRI;
+        pfd[0].events = POLLIN | POLLPRI | POLLERR | POLLHUP | POLLNVAL;
         if(poll(pfd,1,500)>0){
-            if(pfd[0].revents & POLLPRI) { /* receive out-of-band request from client and deal with it */
+            if(pfd[0].revents & POLLPRI && !(pfd[0].revents & POLLERR || pfd[0].revents & POLLHUP || pfd[0].revents & POLLNVAL)) { /* receive out-of-band request from client and deal with it */
                 memset(msgbuf,0,20);
                 msgret = recv(sock, msgbuf, 10 , MSG_OOB);
                 if (msgret < 1) {
@@ -214,7 +218,7 @@ int g15_recv(lcdnode_t *lcdnode, int sock, char *buf, unsigned int len)
                 }
            	process_client_cmds(lcdnode, sock, msgbuf,len);
             }
-            else if(pfd[0].revents & POLLIN) {
+            else if(pfd[0].revents & POLLIN && !(pfd[0].revents & POLLERR || pfd[0].revents & POLLHUP || pfd[0].revents & POLLNVAL)) {
 
                 retval = recv(sock, buf+total, bytesleft, 0);
                 if (retval < 1) { 
@@ -222,6 +226,10 @@ int g15_recv(lcdnode_t *lcdnode, int sock, char *buf, unsigned int len)
                 }
                 total += retval;
                 bytesleft -= retval;
+            }
+            if((pfd[0].revents & POLLERR || pfd[0].revents & POLLHUP || pfd[0].revents & POLLNVAL)){
+               retval=-1;
+               break;
             }
         }
     }
