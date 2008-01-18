4ef2ca3228ad3fcda7770e3d314ead15a29d79c1
mlampard <mlampard@ab83d0d9-5618-0410-a047-8efd564e0644>
2008-01-18 08:49:56 +0000

    g15stats: add network interface information screen.
    git-svn-id: https://g15daemon.svn.sourceforge.net/svnroot/g15daemon/trunk@429 ab83d0d9-5618-0410-a047-8efd564e0644

--- g15daemon-clients/g15stats/g15stats.c
+++ g15daemon-clients/g15stats/g15stats.c
@@ -56,6 +56,16 @@ int leaving=0;
 int g15screen_fd;
 int cycle = 0;
 
+unsigned int net_hist[100][2];
+int net_rr_index=0;
+unsigned long net_max_in=0;
+unsigned long net_max_out=0;
+
+unsigned long maxi(unsigned long a, unsigned long b) {
+  if(a>b)
+    return a;
+  return b;
+}
 
 void drawBar_reversed (g15canvas * canvas, int x1, int y1, int x2, int y2, int color,
                        int num, int max, int type)
@@ -103,7 +113,6 @@ void drawBar_reversed (g15canvas * canvas, int x1, int y1, int x2, int y2, int c
             g15r_drawLine (canvas, x, y1, x, y2, color^1);
     }
 }
-
 
 int daemonise(int nochdir, int noclose) {
     pid_t pid;
@@ -272,6 +281,88 @@ void draw_cpu_screen(g15canvas *canvas) {
 
 }
 
+char * show_bytes(unsigned long bytes) {
+    static char tmpstr[128];
+    if(bytes>1024*1024) {
+      bytes = bytes / (1024*1024);
+      sprintf(tmpstr,"%liMb",bytes);
+    }
+    else if(bytes<1024*1024 && bytes > 1024) {
+      bytes = bytes / 1024;
+      sprintf(tmpstr,"%likb",bytes);
+    }
+    else if(bytes<1024) {
+      sprintf(tmpstr,"%lib",bytes);
+    }
+    return tmpstr;
+}
+
+void draw_net_screen(g15canvas *canvas, char *interface) {
+    int i;
+    int x=0;
+    char tmpstr[128];
+    x=0;
+    float diff=0;
+    float height=0;
+    float last=0;
+    
+    glibtop_netload netload;
+    glibtop_get_netload(&netload,interface);    
+    // in
+    x=53;
+    g15r_clearScreen (canvas, G15_COLOR_WHITE);
+    for(i=net_rr_index+1;i<99;i++) {
+      diff = (float) net_max_in / (float) net_hist[i][0];
+      height = 16-(16/diff);
+      g15r_setPixel(canvas,x,height,1);
+      g15r_drawLine(canvas,x,height,x-1,last,1);
+      last=height;
+      x++;
+    }
+    for(i=0;i<net_rr_index;i++) {
+      diff = (float) net_max_in / (float) net_hist[i][0];
+      height = 16-(16 / diff);
+      g15r_drawLine(canvas,x,height,x-1,last,1);
+      last=height;
+      x++;
+    }
+    // out
+    x=53;
+    for(i=net_rr_index+1;i<99;i++) {
+      diff = (float) net_max_in / (float) net_hist[i][1];
+      height = 34-(16/diff);
+      g15r_setPixel(canvas,x,height,1);
+      g15r_drawLine(canvas,x,height,x-1,last,1);
+      last=height;
+      x++;
+    }
+    for(i=0;i<net_rr_index;i++) {
+      diff = (float) net_max_in / (float) net_hist[i][1];
+      height = 34-(16 / diff);
+      g15r_drawLine(canvas,x,height,x-1,last,1);
+      last=height;
+      x++;
+    }
+    g15r_drawLine (canvas, 52, 0, 52, 34, 1);
+    g15r_drawLine (canvas, 53, 0, 53, 34, 1);
+    g15r_drawLine (canvas, 54, 0, 54, 34, 1);
+
+    sprintf(tmpstr,"IN %s",show_bytes(netload.bytes_in));
+    g15r_renderString (canvas, (unsigned char*)tmpstr, 0, G15_TEXT_MED, 1, 2);
+    sprintf(tmpstr,"OUT %s",show_bytes(netload.bytes_out));
+    g15r_renderString (canvas, (unsigned char*)tmpstr, 0, G15_TEXT_MED, 1, 26);
+
+    sprintf(tmpstr,"%s",interface);
+    g15r_renderString (canvas, (unsigned char*)tmpstr, 0, G15_TEXT_LARGE, 25-(strlen(tmpstr)*9)/2, 14);
+    
+    sprintf(tmpstr,"Peak IN %s/s|",show_bytes(net_max_in));
+    strcat(tmpstr,"Peak OUT ");
+    strcat(tmpstr,show_bytes(net_max_out));
+    strcat(tmpstr,"/s");
+    g15r_renderString (canvas, (unsigned char*)tmpstr, 0, G15_TEXT_SMALL, 80-(strlen(tmpstr)*4)/2, 36);
+
+}
+
 void keyboard_watch(void) {
     unsigned int keystate;
 
@@ -291,6 +382,7 @@ void keyboard_watch(void) {
             cycle=2;
         }
         else if(keystate & G15_KEY_L5) {
+            cycle=3;
         }
         usleep(100*900);
     }
@@ -298,12 +390,43 @@ void keyboard_watch(void) {
     return;
 }
 
+void network_watch(void *iface) {
+  char *interface = (char*)iface;
+  int i=0;
+  glibtop_netload netload;
+  static unsigned long previous_in;
+  static unsigned long previous_out;
+
+  while(leaving==0) {
+    glibtop_get_netload(&netload,interface);
+    if(previous_in+previous_out==0)
+      goto last;
+      
+    net_hist[i][0] = netload.bytes_in-previous_in;
+    net_hist[i][1] = netload.bytes_out-previous_out;
+    net_max_in = maxi(net_max_in,netload.bytes_in-previous_in);
+    net_max_out = maxi(net_max_out,netload.bytes_out-previous_out);
+
+    net_rr_index=i;    
+    i++; if(i>99) i=0;
+    
+    sleep (1);
+    last:
+    previous_in = netload.bytes_in;
+    previous_out = netload.bytes_out;
+
+  }
+}
+
 int main(int argc, char *argv[]){
 
     g15canvas *canvas;
     pthread_t keys_thread;
+    pthread_t net_thread;
     int i;
     int go_daemon=0;
+    int have_nic=0;
+    unsigned char interface[128];
 
     for (i=0;i<argc;i++) {
         if(0==strncmp(argv[i],"-d",2)||0==strncmp(argv[i],"--daemon",8)) {
@@ -316,6 +439,14 @@ int main(int argc, char *argv[]){
             printf("--help (-h) this help text.\n");
             return 0;
         }
+        if(0==strncmp(argv[i],"-i",2)||0==strncmp(argv[i],"--interface",8)) {
+          if(argv[i+1]!=NULL) {
+            have_nic=1;
+            i++;
+            strncpy((char*)interface,argv[i],128);
+          }
+        }
+        
     }        
     if((g15screen_fd = new_g15_screen(G15_G15RBUF))<0){
         printf("Sorry, cant connect to the G15daemon\n");
@@ -331,20 +462,31 @@ int main(int argc, char *argv[]){
     else
         return -1;
 
-    pthread_create(&keys_thread,NULL,(void*)keyboard_watch,NULL);
     glibtop_init();
-
+    pthread_create(&keys_thread,NULL,(void*)keyboard_watch,NULL);
+  
+    if(have_nic==1)
+      pthread_create(&net_thread,NULL,(void*)network_watch,&interface);
+    
     while(leaving==0) {
 
         switch(cycle) {
             case 0:
-                draw_cpu_screen(canvas);
+               draw_cpu_screen(canvas);
                 break;
             case 1:   
                 draw_mem_screen(canvas);
                 break;
             case 2:
                 draw_swap_screen(canvas);
+                break;
+            case 3:
+                if(have_nic==1)
+                  draw_net_screen(canvas,(char*)interface);
+                else {
+                  printf("Please set the interface on the cmdline\n");
+                  cycle=0;
+                }
                 break;
             default:
                 printf("cycle reched %i\n",cycle);
