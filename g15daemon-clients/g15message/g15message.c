9d609690200b34803636074b29ca98740a6f7d53
mlampard <mlampard@ab83d0d9-5618-0410-a047-8efd564e0644>
2009-04-24 07:36:14 +0000

    update g15message changelog
    git-svn-id: https://g15daemon.svn.sourceforge.net/svnroot/g15daemon/trunk@491 ab83d0d9-5618-0410-a047-8efd564e0644

--- g15daemon-clients/g15message/g15message.c
+++ g15daemon-clients/g15message/g15message.c
@@ -15,7 +15,7 @@
         along with g15daemon; if not, write to the Free Software
         Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-        (c) 2006-2007 Mike Lampard 
+        (c) 2006-2009 Mike Lampard 
 
         $Revision$ -  $Date$ $Author$
 
